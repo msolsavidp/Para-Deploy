27c631977a6acd361275e248c15afc552b89467b
msolsavidp <69515039+msolsavidp@users.noreply.github.com>
2023-03-03 16:02:57 -0300

    new backend

--- PF_WinesClubFront/client/src/index.js
+++ PF_WinesClubFront/client/src/index.js
@@ -14,7 +14,7 @@ import axios from "axios";
 // axios.defaults.baseURL = 'http://localhost:3001';
 axios.defaults.headers.common['Authorization'] = sessionStorage.getItem('user');
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://para-deploy-production.up.railway.app/';
 
 const domain = "dev-eqjids43tpn0c5lj.us.auth0.com";
 const clientId = "CW1fFkRrBryYtSpyefTvYFWGf6sNQNhg";
